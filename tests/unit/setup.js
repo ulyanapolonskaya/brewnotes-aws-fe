b859097f44d88c281301bb303e698889b2100fdb
ulyanapolonskaya <ulyana.polonskaya@gmail.com>
2025-04-07 21:28:14 +0200

    added unit tests

---
+++ tests/unit/setup.js
@@ -0,0 +1,8 @@
+// This file contains setup code that will be run before each test
+// You can add global test setup here, such as:
+// - Mock implementations
+// - Global test variables
+// - Custom matchers
+
+// For example:
+// jest.setTimeout(10000); // Increase timeout for all tests
